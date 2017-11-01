a6fefb711a851e9a2865ccc8f1f928705bdda549
Sorogovets.Nikolay <sorogovets.nikolay@gmail.com>
2017-11-02 00:14:42 +0300

    set State crashed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import News from './News';
 import ShoppingList from './ShoppingList';
 
-const my_news = [];
-// const my_news = [
-//     {
-//         author: 'Саша Печкин',
-//         text: 'В четверг, четвертого числа...'
-//     },
-//     {
-//         author: 'Просто Вася',
-//         text: 'Считаю, что $ должен стоить 35 рублей!'
-//     },
-//     {
-//         author: 'Гость',
-//         text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
-//     }
-// ];
+// const my_news = [];
+const my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h3>News</h3>
         <News data={my_news}/>
         <ShoppingList/>
       </div>
--- src/News.js
+++ src/News.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { visible: false };
+    }
+
+    readMore(event) {
+        event.preventDefault();
+        this.setState({ visible: true });
+    }
+
+    render() {
+        const { author, text, bigText } = this.props.data;
+        const visible = this.state.visible;
+        return (
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className="news__text">{text}</p>
+                <a href="#"
+                    onClick={this.readMore}
+                    className={"news__readmore " + (visible ? 'none' : '')}>
+                    More...
+                </a>
+                <p className={"news__big-text " + (visible ? '' : 'none')}>{bigText}</p>
+            </div>
+        )
+    }
+}
+
+Article.propTypes = {
+    data: PropTypes.shape({
+        author: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+        bigText: PropTypes.string.isRequired,
+    })
+}
 
 class News extends Component {
     render() {
         const data = this.props.data;
         let newsTemplate;
-        if(data.length > 0) {
+        if (data.length > 0) {
             newsTemplate = data.map((item, index) => {
                 return (
                     <div key={index}>
-                        <p className="news__author">{item.author}:</p>
-                        <p className="news__text">{item.text}</p>
+                        <Article data={item} />
                     </div>
                 )
             });
@@ -19,11 +56,12 @@ class News extends Component {
         return (
             <div className="news">
                 {newsTemplate}
-                <strong className={data.length ? '' : 'none'}>All news count: {data.length}</strong>
+                <strong className={data.length ? 'news__count' : 'none'}>All news count: {data.length}</strong>
             </div>
         );
     }
 }
-
-class Acticle extends 
+News.propTypes = {
+    data: PropTypes.array.isRequired
+};
 export default News;
